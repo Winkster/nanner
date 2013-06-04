59e97e201e523fefd3be5295af90b2322ea21baf
Winkster <drtubb@gmail.com>
2013-06-04 08:05:02 -0700

    initial commit

---
+++ nanner/src/com/example/nanner/DisplayMessageActivity.java
@@ -0,0 +1,82 @@
+package com.example.nanner;
+
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class DisplayMessageActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		int iv = R.layout.activity_display_message;
+		setContentView(iv);
+		// Show the Up button in the action bar.
+//		setupActionBar();
+		TextView tv = new TextView(this);
+		tv.setText("Simply dummy text");
+        // finally add your TextView to the RelativeLayout
+        iv.addView(tv);
+
+		
+//		textView1.setText("fuck off!");
+/*		
+		
+		super.onCreate(savedInstanceState);
+
+	    // Get the message from the intent
+	    Intent intent = getIntent();
+	    String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+	    // Create the text view
+	    TextView textView = new TextView(this);
+	    textView.setTextSize(40);
+	    if (message.equalsIgnoreCase("monster")) 
+	    	{ textView.setText("NANNER!"); }
+	    else { textView.setText("Nope, try again."); }	    
+	    	
+	    // Set the text view as the activity layout
+	    setContentView(textView);
+*/
+	}
+
+	/**
+	 * Set up the {@link android.app.ActionBar}, if the API is available.
+	 */
+	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+	private void setupActionBar() {
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+			getActionBar().setDisplayHomeAsUpEnabled(true);
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.display_message, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			// This ID represents the Home or Up button. In the case of this
+			// activity, the Up button is shown. Use NavUtils to allow users
+			// to navigate up one level in the application structure. For
+			// more details, see the Navigation pattern on Android Design:
+			//
+			// http://developer.android.com/design/patterns/navigation.html#up-vs-back
+			//
+			NavUtils.navigateUpFromSameTask(this);
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+}
